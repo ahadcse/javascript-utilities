806b9e494af650fbf05869d3cbed47a70179859f
Abdullah Al Ahad <ahadcse@gmail.com>
2019-10-15 11:23:40 +0200

    check arrar for strings
    Checks whether a bunch of particular string is available in an array or not

--- utilityExamples.js
+++ utilityExamples.js
@@ -1,19 +1,20 @@
 const getOnlyUniqueByHighestTimestamp = (acc = [], curr = {}) => {
 	const index = acc.findIndex((item) => item.productId === curr.productId)
-
 	if (!acc[index]) {
 		return [...acc, curr]
 	}
-
 	if (acc[index].timestamp < curr.timestamp) {
 		acc[index] = curr
 	}
-
 	return acc
 }
-
 // console.log('Unique by highest timestamp: ', getOnlyUniqueByHighestTimestamp(inputWithHighest, { "productId": 1, "timestamp": 3 }));
 
 const skus = "DW00100038, DW00100037"
 const makeSkuArray = (skus) => skus.split(',').map((r) => `'${r.trim()}'`)
 // console.log(`AND items.Sku IN (${ makeSkuArray(skus) })`)
+
+const checkForItemInArray = (it) => {
+	const arrayToCheck = ['integration-28', 'integration-30', 'integration-31']
+	return arrayToCheck.some(item => it.includes(item))
+}
